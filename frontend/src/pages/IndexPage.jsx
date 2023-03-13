8edef0484d6afd6663aa3acb71d698d3ab5e854b
Vince Lim <vince_victor_lim@dlsu.edu.ph>
2023-03-13 17:11:17 +0800

    Update

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -13,13 +13,16 @@ export default function IndexPage() {
             <form action="../../student/post" method="post" className="form">
                 <button type="submit">Connected?</button> {/* Sends a console.log in backend to verify if connected to React*/}
             </form>
-            <a href='/login'> Go to Login Page</a>
-            <a href='/register'> Go to Register Page</a>
-            <a href='/enrolldash'> Go to Enroll Dashboard</a>
-            <a href='/notif'> Go to Notif Page</a>
-            <a href ='/schedpage'>Go to Sched Page</a>
-            <a href='/adminrec'>Go to Student Records</a>
-            <a href='/teacher'>Go to Teacher Records</a>
+            <a href='/login'> Go to Login Page</a><br></br>
+            <a href='/register'> Go to Register Page</a><br></br>
+            <a href='/enrolldash'> Go to Enroll Dashboard</a><br></br>
+            <a href='/notif'> Go to Notif Page</a><br></br>
+            <a href ='/schedpage'>Go to Sched Page</a><br></br>
+            <a href='/adminrec'>Go to Student Records</a><br></br>
+            <a href='/teacher'>Go to Teacher Records</a><br></br>
+            <a href='/payroll'>Go to Payroll</a><br></br>
+            <a href='/studentRecord'>Go to Student Records</a><br></br>
+            <a href='/enrollpending'>Go to Pending Enrollments</a><br></br>
         </div>
          
             
---
+++ frontend/src/pages/StudentRecord.jsx
@@ -0,0 +1,56 @@
+import Navbar from '../components/Navbar_top'
+import Sidebar from '../components/Sidebar';
+
+export default function StudentRecord() {
+    return(
+        <div className='with-sidebar'>
+            <Sidebar/>
+            <div className = 'content-container'>
+                <h1>Student Records</h1>
+                <div className='table-container2'>
+                    <table cellSpacing={0}>
+                         <tr className='table-headers'>
+                            <td>Schedule</td>
+                         </tr>
+                         <tr className='table-headers2'>
+                            <td>Day/s</td>
+                            <td>Program</td>
+                            <td>Time</td>
+                            <td>Status</td>
+                            <td>Student Enrolled</td>
+                         </tr>
+                         <tr>
+                            <td>MW</td>
+                            <td>1 Hour</td>
+                            <td>HH:MM - HH:MM PHT</td>
+                            <td>Occupied</td>
+                            <td>First Name, Last Name</td>
+                         </tr>
+                    </table>
+                </div>
+                <div className='table-container2'>
+                        <table cellSpacing={0}>
+                        <tr className='table-headers'>
+                            <td>Sessions</td>
+                         </tr>
+                         <tr className='table-headers2'>
+                            <td>Number</td>
+                            <td>Day</td>
+                            <td>Date</td>
+                            <td>Time</td>
+                            <td>Student Attendance</td>
+                         </tr>
+                         <tr>
+                            <td>1</td>
+                            <td>Monday</td>
+                            <td>MM/DD/YYYY</td>
+                            <td>HH:MM - HH:MM PHT</td>
+                            <td>Present</td>
+                         </tr>
+                        </table>
+                    </div>
+            </div>
+        </div>
+    )
+
+}
