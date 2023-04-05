7107aadc08c880b155c80e9af1f08326ea674047
Vince Lim <vince_victor_lim@dlsu.edu.ph>
2023-04-05 23:39:41 +0800

    more updates

--- frontend/src/pages/Faculty.jsx
+++ frontend/src/pages/Faculty.jsx
@@ -2,6 +2,10 @@
 import React from 'react';
 import guitar from '../public/assets/Guitar.jpg'
 import drums from '../public/assets/Drums.jpg'
+import piano from '../public/assets/Piano.jpg'
+import violin from '../public/assets/Violin.jpg'
+import trumpet from '../public/assets/Trumpet.jpg'
+import saxophone from '../public/assets/Saxophone.jpg'
 
 import '../public/assets/logo.png'
 import '../public/styles/App.css'
@@ -16,28 +20,30 @@ export default function FacultyPage() {
             </h1>
             <br></br>
             <div className="instrument-container">
-            <div className="instrument-card">
+            <a href="/facultypage">
+                <div className="instrument-card">
                 <img className="instrument-image" src={guitar} alt="Guitar" />
                 <div className="instrument-label">Guitar</div>
             </div>
+            </a>
             <div className="instrument-card">
                 <img className="instrument-image" src={drums} alt="Drums" />
                 <div className="instrument-label">Drums</div>
             </div>
             <div className="instrument-card">
-                <img className="instrument-image" src="piano.jpg" alt="Piano" />
+                <img className="instrument-image" src={piano} alt="Piano" />
                 <div className="instrument-label">Piano</div>
             </div>
             <div className="instrument-card">
-                <img className="instrument-image" src="violin.jpg" alt="Violin" />
+                <img className="instrument-image" src={violin} alt="Violin" />
                 <div className="instrument-label">Violin</div>
             </div>
             <div className="instrument-card">
-                <img className="instrument-image" src="trumpet.jpg" alt="Trumpet" />
+                <img className="instrument-image" src={trumpet} alt="Trumpet" />
                 <div className="instrument-label">Trumpet</div>
             </div>
             <div className="instrument-card">
-                <img className="instrument-image" src="saxophone.jpg" alt="Saxophone" />
+                <img className="instrument-image" src={saxophone} alt="Saxophone" />
                 <div className="instrument-label">Saxophone</div>
             </div>
             </div>
