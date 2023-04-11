41ce7eeaa0a3ef4153f7588735bc70bf531193f8
Vince Lim <vince_victor_lim@dlsu.edu.ph>
2023-04-11 15:12:17 +0800

    Change CSS of calendar and faculty

--- frontend/src/pages/Faculty.jsx
+++ frontend/src/pages/Faculty.jsx
@@ -1,4 +1,5 @@
 
+import { useNavigate } from 'react-router-dom'
 import React from 'react';
 import guitar from '../public/assets/Guitar.jpg'
 import drums from '../public/assets/Drums.jpg'
@@ -12,50 +13,51 @@ import '../public/styles/App.css'
 
 export default function FacultyPage() {
 
-    
+    const navigate = useNavigate()
+
     return(
         <div id='aboutUsHeader'>
             <h1>
             Faculty
             </h1>
             <br></br>
-            <div className="instrument-container">
-            <a href="/facultymembers">
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-container">
+            {/* <a href="/facultymembers"> */}
                 <div className="instrument-card">
                 <img className="instrument-image" src={guitar} alt="Guitar" />
                 <div className="instrument-label">Guitar</div>
             </div>
-            </a>
-            <a href="/facultymembers">
-            <div className="instrument-card">
+            {/* </a> */}
+            {/* <a href="/facultymembers"> */}
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
                 <img className="instrument-image" src={drums} alt="Drums" />
                 <div className="instrument-label">Drums</div>
             </div>
-            </a>
-            <a href="/facultymembers">
-            <div className="instrument-card">
+            {/* </a> */}
+            {/* <a href="/facultymembers"> */}
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
                 <img className="instrument-image" src={piano} alt="Piano" />
                 <div className="instrument-label">Piano</div>
             </div>
-            </a>
-            <a href="/facultymembers">
-            <div className="instrument-card">
+            {/* </a> */}
+            {/* <a href="/facultymembers"> */}
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
                 <img className="instrument-image" src={violin} alt="Violin" />
                 <div className="instrument-label">Violin</div>
             </div>
-            </a>
-            <a href="/facultymembers">
-            <div className="instrument-card">
+            {/* </a> */}
+            {/* <a href="/facultymembers">  */}
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
                 <img className="instrument-image" src={trumpet} alt="Trumpet" />
                 <div className="instrument-label">Trumpet</div>
             </div>
-            </a>
-            <a href="/facultymembers">
-            <div className="instrument-card">
+            {/* </a> */}
+            {/* <a href="/facultymembers"> */}
+            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
                 <img className="instrument-image" src={saxophone} alt="Saxophone" />
                 <div className="instrument-label">Saxophone</div>
             </div>
-            </a>
+            {/* </a> */}
             </div>
 
         </div>
