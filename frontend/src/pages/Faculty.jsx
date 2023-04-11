3d8aea3b2fbae42b37639ae6833ab52cb1e26d1d
Vince Lim <vince_victor_lim@dlsu.edu.ph>
2023-04-11 21:26:48 +0800

    Update Faculty.jsx

--- frontend/src/pages/Faculty.jsx
+++ frontend/src/pages/Faculty.jsx
@@ -21,40 +21,40 @@ export default function FacultyPage() {
             Faculty
             </h1>
             <br></br>
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-container">
+            <div className="instrument-container">
             {/* <a href="/facultymembers"> */}
                 <div className="instrument-card">
-                <img className="instrument-image" src={guitar} alt="Guitar" />
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={guitar} alt="Guitar" />
                 <div className="instrument-label">Guitar</div>
             </div>
             {/* </a> */}
             {/* <a href="/facultymembers"> */}
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
-                <img className="instrument-image" src={drums} alt="Drums" />
+            <div className="instrument-card">
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={drums} alt="Drums" />
                 <div className="instrument-label">Drums</div>
             </div>
             {/* </a> */}
             {/* <a href="/facultymembers"> */}
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
-                <img className="instrument-image" src={piano} alt="Piano" />
+            <div className="instrument-card">
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={piano} alt="Piano" />
                 <div className="instrument-label">Piano</div>
             </div>
             {/* </a> */}
             {/* <a href="/facultymembers"> */}
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
-                <img className="instrument-image" src={violin} alt="Violin" />
+            <div className="instrument-card">
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={violin} alt="Violin" />
                 <div className="instrument-label">Violin</div>
             </div>
             {/* </a> */}
             {/* <a href="/facultymembers">  */}
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
-                <img className="instrument-image" src={trumpet} alt="Trumpet" />
+            <div className="instrument-card">
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={trumpet} alt="Trumpet" />
                 <div className="instrument-label">Trumpet</div>
             </div>
             {/* </a> */}
             {/* <a href="/facultymembers"> */}
-            <div onClick={()=>navigate('/facultymembers')} className="instrument-card">
-                <img className="instrument-image" src={saxophone} alt="Saxophone" />
+            <div className="instrument-card">
+                <img onClick={()=>navigate('/facultymembers')} className="instrument-image" src={saxophone} alt="Saxophone" />
                 <div className="instrument-label">Saxophone</div>
             </div>
             {/* </a> */}
